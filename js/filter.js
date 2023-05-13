a17bec857ec7f886aefcf54a11386b16024a8691
Lokesh Sequeira <Lokesh.Sequeira@H5datacenters.com>
2023-05-13 07:44:25 -0400

    changes

--- js/filter.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#searchInput').on('keyup', function(){
-        var value = $(this).val().toLowerCase();
-        $('#log-table-data tr').filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
