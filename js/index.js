6ca0ca72b87722542d4b3b544398952623491b7c
Lokesh Sequeira <Lokesh.Sequeira@H5datacenters.com>
2023-05-26 15:47:48 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
 $(function () {
 
-    $('#checkin-btn').click(function(e) {
+    let checkedIn = false;
+
+    $('#checkIn-name').on('keyup', function(e) {
         e.preventDefault();
-
-        let name = $('#checkin-name').val();
-        let checkIn = false;
-        let errText = $('#error');
-
-        if (name === "") {
-            errText.show().text('* name required');
+        if($(this).val() === "") {
+            $('#checkIn-btn').hide();
+        } else if ($(this).val().length < 3 || $(this).val().length > 17) {
+            $('#checkIn-btn').hide();
+        }else {
+            $('#checkIn-btn').fadeIn();
         }
+    })
 
+    $('#checkIn-btn').click(function(e) {
+        e.preventDefault();
+        checkedIn = true;
+        $('#checklistContainer').slideDown('slow');
+        $(this).hide(800);
+        $('#checkIn-title').text('Checked In')
+    })
+
+    $('#checklist-btn').click(function(e) {
+        e.preventDefault();
+        let checkboxes = $('.checkbox');
+        let checked = [];
+        let detailsBtns = $('.toggle-details-btn');
+        let detailBoxes = $('.details-box');
+
+        $('#checklistContainer').slideUp(800);
+        $('#checkIn-name').val('');
+        $('#checkIn-title').text('Check In');
+
+        checkboxes.each(checkbox () => {
+            console.log(checkbox);
+        })
     })
 
 })
